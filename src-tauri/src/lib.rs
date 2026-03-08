9e895e1117205f91eba48b49aa2f0456c5a18c86
larrybwosi <larrybwosi@gmail.com>
2026-03-08 20:43:07 +0300

    fix(build): posthog env

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -332,7 +332,6 @@ pub fn run() {
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_shell::init())
-        // .plugin(tauri_plugin_aptabase::init(...))
         .plugin(tauri_plugin_better_posthog::init())
         .invoke_handler(tauri::generate_handler![
             scanner_manager::start_scan,
